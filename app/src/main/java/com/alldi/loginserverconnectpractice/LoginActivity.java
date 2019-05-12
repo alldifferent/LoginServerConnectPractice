657386cd8af83288e27bb4cd66febc7850189dd1
somsap <rome777@gmail.com>
2019-05-12 14:07:28 +0900

    SharedPrefrenence 예제  - 로그인버튼이 눌리면 입력되어있던 ID를 저장  - 앱이 켜질때마다 마지막으로 저장된 ID를 저장

--- app/src/main/java/com/alldi/loginserverconnectpractice/LoginActivity.java
+++ app/src/main/java/com/alldi/loginserverconnectpractice/LoginActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.alldi.loginserverconnectpractice.databinding.ActivityLoginBinding;
+import com.alldi.loginserverconnectpractice.utils.ConnectServer;
+import com.alldi.loginserverconnectpractice.utils.ContextUtil;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends BaseActivity {
 
@@ -24,8 +28,22 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
 //                1. 아이디와 비번을 뭐라고 입력했는지? 받아오기
+
+                String inputId = act.loginIdEdt.getText().toString();
+                String inputPw = act.loginPwEdt.getText().toString();
+
+//                1.2 - 입력받은 ID를 SharedPreferences에 저장.
+                ContextUtil.setUserInputId(mContext, inputId);
+
 //                2. 받아온 아이디와 비번이 정말로 올바른 회원인지? 검사
 //                  아이디/비번이 모두 동일한 사람이 회원 명부에 있는지 검사
+
+                ConnectServer.postRequestSignIn(mContext, inputId, inputPw, new ConnectServer.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+                    }
+                });
 
 //                test123 / test123
 //                iu0001 / Test!123
@@ -41,7 +59,8 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        String savedUserId = ContextUtil.getUserInputId(mContext);
+        act.loginIdEdt.setText(savedUserId);
     }
 
     @Override
